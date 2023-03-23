1c327f1600948e1002efefccd4edb2c8955678b6
TeckWIT <114758496+TeckZin@users.noreply.github.com>
2023-03-23 01:21:52 -0400

    name changes

--- src/main/java/com/example/cookieclicker/maincontroller.java
+++ src/main/java/com/example/cookieclicker/maincontroller.java
@@ -3,7 +3,7 @@ package com.example.cookieclicker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class maincontroller {
 
     int i = 0;
     @FXML

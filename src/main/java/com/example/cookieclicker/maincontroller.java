f7ad42044b392828fad05f193c8d2a1c0724998c
TeckWIT <114758496+TeckZin@users.noreply.github.com>
2023-03-30 18:04:14 -0400

    commited

--- src/main/java/com/example/cookieclicker/maincontroller.java
+++ src/main/java/com/example/cookieclicker/maincontroller.java
@@ -3,23 +3,61 @@ package com.example.cookieclicker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.Random;
+
 public class maincontroller {
 
+    int limit = 0;
     int i = 0;
+    int goalNum;
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Label goal;
+
+    @FXML
+    private Label randomLimit;
+
+    @FXML
+    private Label goalReach;
+
+    private Random rd = new Random();
 
     @FXML
     protected void onCookieButtonClick() {
         i++;
         String s = String.format("%d", i);
         welcomeText.setText(s);
+        if(i == goalNum){
+            goalReach.setText("GOAL REACH");
+        }
     }
 
     @FXML
     protected void onResetButtonClick(){
-        i = 0;
-        String s = String.format("%d", i);
-        welcomeText.setText(s);
+        goalNum = rd.nextInt(limit);
+        String s = String.format("%d", goalNum);
+        goal.setText(s);
     }
+    @FXML
+    protected void plusGoalButton(){
+        limit += 10;
+        String s = String.format("%d", limit);
+        randomLimit.setText(s);
+    }
+
+    @FXML
+    protected void minusGoalButton(){
+        limit -= 10;
+        String s = String.format("%d", limit);
+        randomLimit.setText(s);
+    }
+
+
+
+
+
+
+
 }
